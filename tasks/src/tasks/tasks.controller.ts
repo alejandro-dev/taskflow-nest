31700ed8f69b60a330c613a41ba11a90d009dc11
Alejandro <alejandroalvareznistal.dev@gmail.com>
2025-02-26 20:58:50 +0100

    comments: tasks comments

--- tasks/src/tasks/tasks.controller.ts
+++ tasks/src/tasks/tasks.controller.ts
@@ -8,41 +8,277 @@ import { MessagePattern, Payload } from '@nestjs/microservices';
 export class TasksController {
    constructor(private readonly tasksService: TasksService) {}
 
+   /**
+    * 
+    * @returns {{ healt: boolean }} The health status of the service
+    * @messagePattern tasks.healt
+    * @description Healthcheck endpoint to check if the service is up and running
+    *
+    */
    @MessagePattern({ cmd: 'healt_tasks' })
-   healt() {
+   healt(): { healt: boolean } {
       return { healt: true };
    }
 
+   /**
+    * 
+    * @messagePattern tasks.create
+    * @description Create a new task
+    * @param createTaskDto - The task data to create a new task 
+    * @param createTaskDto.title - The title of the task
+    * @param createTaskDto.description - The description of the task
+    * @param createTaskDto.assignedTo - The id of the user assigned to the task
+    * @param createTaskDto.dueDate - The due date of the task
+    * @param createTaskDto.status - The status of the task
+    * @param createTaskDto.priority - The priority of the task
+    * 
+    * @returns {Object} The response contain the operation status and the created task
+    * 
+    * @example
+    * // Example success response
+    * statusCode: 200
+    * {
+    *    "status": "success",
+    *    "message": "Task created successfully",
+    *    "task": {
+    *      "id": "1234567890abcdef12345678",
+    *      "title": "Task title",
+    *      "description": "Task description",
+    *      "assignedTo": "1234567890abcdef12345678",
+    *      "dueDate": "2025-02-25T00:00:00.000Z",
+    *      "status": "pendiente",
+    *      "priority": "media",
+    *      "createdAt": "2025-02-25T00:00:00.000Z",
+    *      "updatedAt": "2025-02-25T00:00:00.000Z" 
+    *    }
+    * }   
+    *   
+    * @example
+    * // Example bad request response
+    * statusCode: 400
+    * {
+    *    "status": "fail",
+    *    "error": "Your request is invalid",
+    *    "message": [
+    *      "title is not allowed to be empty",
+    *      "title must be a string"
+    *    ]
+    * }  
+    * 
+    * @example
+    * // Internal Server Error response
+    * statusCode: 500
+    * {
+    *    "status": "error",
+    *    "message": "Internal Server Error"
+    * }
+    * 
+    */
    @MessagePattern({ cmd: 'tasks.create' })
-   create(@Payload() createTaskDto: CreateTaskDto) {
-      console.log('log5');
-      return this.tasksService.create(createTaskDto);
-   }
-
-   @MessagePattern({ cmd: 'tasks.findAll' })
-   findAll() {
-      return this.tasksService.findAll();
-   }
-
-   @MessagePattern({ cmd: 'tasks.findOne' })
-   findOne(@Payload('id') id: string) {
+   create(@Payload() createTaskDto: CreateTaskDto): Object{
       try {
-         return this.tasksService.findOne(id);
+         return  this.tasksService.create(createTaskDto);
 
       } catch (error) {
-         console.log('log4');
          return error;
       }
    }
 
-   @MessagePattern({ cmd: 'tasks.update' })
-   // update(@Payload('id') id: string, @Body() updateTaskDto: UpdateTaskDto) {
-   update(@Payload() updateTaskDto: UpdateTaskDto) {
-      return this.tasksService.update(updateTaskDto);
+   /**
+    * 
+    * @returns {Object} The response contain the operation status and the list of tasks
+    * 
+    * @messagePattern tasks.findAll
+    * @description Get all tasks
+    * 
+    * @example
+    * // Example success response
+    * statusCode: 200
+    * {
+    *    "status": "success",
+    *    "tasks": [
+    *      {
+    *        "id": "1234567890abcdef12345678",
+    *        "title": "Task title",
+    *        "description": "Task description",
+    *        "assignedTo": "1234567890abcdef12345678",
+    *        "dueDate": "2025-02-25T00:00:00.000Z",
+    *        "status": "pendiente",
+    *        "priority": "media",
+    *        "createdAt": "2025-02-25T00:00:00.000Z",
+    *        "updatedAt": "2025-02-25T00:00:00.000Z"
+    *      }
+    *    ]
+    * } 
+    * 
+    * 
+    * @example
+    * // Internal Server Error response
+    * statusCode: 500
+    * {
+    *    "status": "error",
+    *    "message": "Internal Server Error"
+    * }
+    * 
+    */
+   @MessagePattern({ cmd: 'tasks.findAll' })
+   findAll(): Object {
+      try {
+         return this.tasksService.findAll();
+
+      } catch (error) {
+         return error;
+      }
    }
 
+   /**
+    * 
+    * @returns {Object} The response contain the operation status and the task
+    * 
+    * @messagePattern tasks.findOne
+    * @description Get a task by id
+    * @param id - The id of the task
+    * 
+    * @example
+    * // Example success response
+    * statusCode: 200
+    * {
+    *    "status": "success",
+    *    "task": {
+    *      "id": "1234567890abcdef12345678",
+    *      "title": "Task title",
+    *      "description": "Task description",
+    *      "assignedTo": "1234567890abcdef12345678",
+    *      "dueDate": "2025-02-25T00:00:00.000Z",
+    *      "status": "pendiente",
+    *      "priority": "media",
+    *      "createdAt": "2025-02-25T00:00:00.000Z",
+    *      "updatedAt": "2025-02-25T00:00:00.000Z"
+    *    }
+    * }
+    *   
+    * @example
+    * // Internal Server Error response
+    * statusCode: 500
+    * {
+    *    "status": "error",
+    *    "message": "Internal Server Error"
+    * }
+    * 
+    */
+   @MessagePattern({ cmd: 'tasks.findOne' })
+   findOne(@Payload('id') id: string): Object {
+      try {
+         return this.tasksService.findOne(id);
+
+      } catch (error) {
+         return error;
+      }
+   }
+
+   /**
+    * 
+    * @param updateTaskDto - The task data to update a task
+    * @param updateTaskDto.id - The id of the task
+    * @param updateTaskDto.title - The title of the task
+    * @param updateTaskDto.description - The description of the task
+    * @param updateTaskDto.assignedTo - The id of the user assigned to the task
+    * @param updateTaskDto.dueDate - The due date of the task
+    * @param updateTaskDto.status - The status of the task
+    * @param updateTaskDto.priority - The priority of the task
+    * @returns {Object} The response contain the operation status and the updated task
+    * 
+    * @messagePattern tasks.update
+    * @description Update a task by id
+    * 
+    * @example
+    * // Example success response
+    * statusCode: 200
+    * {
+    *    "status": "success",
+    *    "task": {
+    *      "id": "1234567890abcdef12345678",
+    *      "title": "Task title"
+    *    }
+    * }
+    * 
+    * @example
+    * // Example bad request response
+    * statusCode: 400
+    * {
+    *    "status": "fail",
+    *    "error": "Your request is invalid",
+    *    "message": [
+    *      "title is not allowed to be empty",
+    *      "title must be a string"
+    *    ]
+    * } 
+    * 
+    * @example
+    * // Not found task response
+    * statusCode: 404
+    * {
+    *    "status": "fail",
+    *    "message": "Task not found"
+    * }
+    * 
+    * @example
+    * // Internal Server Error response
+    * statusCode: 500
+    * {
+    *    "status": "error",
+    *    "message": "Internal Server Error"
+    * }
+    */
+   @MessagePattern({ cmd: 'tasks.update' })
+   update(@Payload() updateTaskDto: UpdateTaskDto): Object {
+      try {
+         return this.tasksService.update(updateTaskDto);
+
+      } catch (error) {
+         return error;
+      }
+   }
+
+   /**
+    * 
+    * @returns {Object} The response contain the operation status and the message
+    * 
+    * @messagePattern tasks.delete
+    * @description Delete a task by id
+    * 
+    * @example
+    * // Example success response
+    * statusCode: 200
+    * {
+    *    "status": "success",
+    *    "message": "The task #1234567890abcdef12345678 has been deleted"
+    * }
+    * 
+    * @example
+    * // Not found task response
+    * statusCode: 404
+    * {
+    *    "status": "fail",
+    *    "message": "Task not found"
+    * }
+    * 
+    * @example
+    * // Internal Server Error response
+    * statusCode: 500
+    * {
+    *    "status": "error",
+    *    "message": "Internal Server Error"
+    * }
+    * 
+    */
    @MessagePattern({ cmd: 'tasks.delete' })
-   remove(@Payload('id') id: string) {
-      return this.tasksService.remove(id);
+   remove(@Payload('id') id: string): Object {
+      try {
+         return this.tasksService.remove(id);
+
+      } catch (error) {
+         return error;
+      }
    }
 }
--- tasks/src/tasks/tasks.service.ts
+++ tasks/src/tasks/tasks.service.ts
@@ -11,45 +11,58 @@ export class TasksService {
 
    /**
     * 
-    * @param createTaskDto 
-    * @returns 
+    * @description Create a new task
+    * @param createTaskDto - The task data to create a new task 
+    * @param createTaskDto.title - The title of the task
+    * @param createTaskDto.description - The description of the task
+    * @param createTaskDto.assignedTo - The id of the user assigned to the task
+    * @param createTaskDto.dueDate - The due date of the task
+    * @param createTaskDto.status - The status of the task
+    * @param createTaskDto.priority - The priority of the task
+    * 
+    * @returns {Promise<Object | any>} The response contain the operation status and the created task
     * 
     * @example
+    * // Example success response
+    * statusCode: 200
     * {
-    *     "title": "Task title",
-    *     "description": "Task description",
-    *     "assignedTo": "1234567890abcdef12345678",
-    *     "dueDate": "2025-02-25T00:00:00.000Z",
-    *     "status": "pendiente",
-    *     "priority": "media"
-    * }
+    *    "status": "success",
+    *    "message": "Task created successfully",
+    *    "task": {
+    *      "id": "1234567890abcdef12345678",
+    *      "title": "Task title",
+    *      "description": "Task description",
+    *      "assignedTo": "1234567890abcdef12345678",
+    *      "dueDate": "2025-02-25T00:00:00.000Z",
+    *      "status": "pendiente",
+    *      "priority": "media",
+    *      "createdAt": "2025-02-25T00:00:00.000Z",
+    *      "updatedAt": "2025-02-25T00:00:00.000Z" 
+    *    }
+    * }   
+    *   
     * @example
+    * // Example bad request response
+    * statusCode: 400
     * {
-    *     "status": "success",
-    *     "task": {
-    *         "id": "1234567890abcdef12345678",
-    *         "title": "Task title",
-    *         "description": "Task description",
-    *         "assignedTo": "1234567890abcdef12345678",
-    *         "dueDate": "2025-02-25T00:00:00.000Z",
-    *         "status": "pendiente",
-    *         "priority": "media",
-    *         "createdAt": "2025-02-25T00:00:00.000Z",
-    *         "updatedAt": "2025-02-25T00:00:00.000Z"
-    *     },
-    *     "message": "Task created successfully"
-    * }
+    *    "status": "fail",
+    *    "error": "Your request is invalid",
+    *    "message": [
+    *      "title is not allowed to be empty",
+    *      "title must be a string"
+    *    ]
+    * }  
+    * 
     * @example
+    * // Internal Server Error response
+    * statusCode: 500
     * {
-    *     "statusCode": 400,
-    *     "error": "Bad Request",
-    *     "message": [
-    *         "title is not allowed to be empty",
-    *         "title must be a string"
-    *     ]
+    *    "status": "error",
+    *    "message": "Internal Server Error"
     * }
+    * 
     */
-   async create(createTaskDto: CreateTaskDto) {
+   async create(createTaskDto: CreateTaskDto): Promise<Object | any> {
       try {
          const task = await this.prisma.task.create({
             data: {
@@ -67,77 +80,88 @@ export class TasksService {
    } 
 
    /**
-    *    
-    * @returns 
+    * 
+    * @returns {Promise<Object | any>} The response contain the operation status and the list of tasks
+    * 
+    * @messagePattern tasks.findAll
+    * @description Get all tasks
     * 
     * @example
+    * // Example success response
+    * statusCode: 200
     * {
-    *     "status": "success",
-    *     "tasks": [
-    *         {
-    *             "id": "1234567890abcdef12345678",
-    *             "title": "Task title",
-    *             "description": "Task description",
-    *             "assignedTo": "1234567890abcdef12345678",
-    *             "dueDate": "2025-02-25T00:00:00.000Z",
-    *             "status": "pendiente",
-    *             "priority": "media",
-    *             "createdAt": "2025-02-25T00:00:00.000Z",
-    *             "updatedAt": "2025-02-25T00:00:00.000Z"
-    *         },
-    *         {
-    *             "id": "1234567890abcdef12345678",
-    *             "title": "Task title",
-    *             "description": "Task description",
-    *             "assignedTo": "1234567890abcdef12345678",
-    *             "dueDate": "2025-02-25T00:00:00.000Z",
-    *             "status": "pendiente",
-    *             "priority": "media",
-    *             "createdAt": "2025-02-25T00:00:00.000Z",
-    *             "updatedAt": "2025-02-25T00:00:00.000Z"
-    *         }
-    *     ]
+    *    "status": "success",
+    *    "tasks": [
+    *      {
+    *        "id": "1234567890abcdef12345678",
+    *        "title": "Task title",
+    *        "description": "Task description",
+    *        "assignedTo": "1234567890abcdef12345678",
+    *        "dueDate": "2025-02-25T00:00:00.000Z",
+    *        "status": "pendiente",
+    *        "priority": "media",
+    *        "createdAt": "2025-02-25T00:00:00.000Z",
+    *        "updatedAt": "2025-02-25T00:00:00.000Z"
+    *      }
+    *    ]
     * } 
+    * 
+    * 
+    * @example
+    * // Internal Server Error response
+    * statusCode: 500
+    * {
+    *    "status": "error",
+    *    "message": "Internal Server Error"
+    * }
+    * 
     */
-   async findAll() {
+   async findAll(): Promise<Object | any> {
       try {
          const tasks = await this.prisma.task.findMany();
          return { status: 'success', tasks };
 
       } catch (error) {
-         console.log(error);
          handleRpcError(error);
       }
    }
 
    /**
     * 
-    * @param id 
-    * @returns 
+    * @returns {Promise<Object | any>} The response contain the operation status and the task
+    * 
+    * @messagePattern tasks.findOne
+    * @description Get a task by id
+    * @param id - The id of the task
     * 
     * @example
+    * // Example success response
+    * statusCode: 200
     * {
-    *     "status": "success",
-    *     "task": {
-    *         "id": "1234567890abcdef12345678",
-    *         "title": "Task title",
-    *         "description": "Task description",
-    *         "assignedTo": "1234567890abcdef12345678",
-    *         "dueDate": "2025-02-25T00:00:00.000Z",
-    *         "status": "pendiente",
-    *         "priority": "media",
-    *         "createdAt": "2025-02-25T00:00:00.000Z",
-    *         "updatedAt": "2025-02-25T00:00:00.000Z"
-    *     }
+    *    "status": "success",
+    *    "task": {
+    *      "id": "1234567890abcdef12345678",
+    *      "title": "Task title",
+    *      "description": "Task description",
+    *      "assignedTo": "1234567890abcdef12345678",
+    *      "dueDate": "2025-02-25T00:00:00.000Z",
+    *      "status": "pendiente",
+    *      "priority": "media",
+    *      "createdAt": "2025-02-25T00:00:00.000Z",
+    *      "updatedAt": "2025-02-25T00:00:00.000Z"
+    *    }
     * }
+    *   
     * @example
+    * // Internal Server Error response
+    * statusCode: 500
     * {
-    *     "statusCode": 404,
-    *     "error": "Not Found",
-    *     "message": "Task not found"    
+    *    "status": "error",
+    *    "message": "Internal Server Error"
     * }
+    * 
     */
-   async findOne(id: string) {
+   async findOne(id: string): Promise<Object | any> {
       try {
          const task = await this.prisma.task.findUnique({
             where: {
@@ -159,20 +183,22 @@ export class TasksService {
 
    /**
     * 
-    * @param id 
-    * @param updateTaskDto 
-    * @returns 
+    * @param updateTaskDto - The task data to update a task
+    * @param updateTaskDto.id - The id of the task
+    * @param updateTaskDto.title - The title of the task
+    * @param updateTaskDto.description - The description of the task
+    * @param updateTaskDto.assignedTo - The id of the user assigned to the task
+    * @param updateTaskDto.dueDate - The due date of the task
+    * @param updateTaskDto.status - The status of the task
+    * @param updateTaskDto.priority - The priority of the task
+    * @returns {Promise<Object | any>} The response contain the operation status and the updated task
+    * 
+    * @messagePattern tasks.update
+    * @description Update a task by id
     * 
     * @example
-    * {
-    *    "title": "Task title",
-    *    "description": "Task description",
-    *    "assignedTo": "1234567890abcdef12345678",
-    *    "dueDate": "2025-02-25T00:00:00.000Z",
-    *    "status": "pendiente",
-    *    "priority": "media"
-    * }
-    * @example
+    * // Example success response
+    * statusCode: 200
     * {
     *    "status": "success",
     *    "task": {
@@ -180,24 +206,36 @@ export class TasksService {
     *      "title": "Task title"
     *    }
     * }
+    * 
     * @example
+    * // Example bad request response
+    * statusCode: 400
     * {
-    *    "statusCode": 404,
-    *    "error": "Not Found",
-    *    "message": "Task not found"    
-    * }
-    * @example
-    * {
-    *    "statusCode": 400,
-    *    "error": "Bad Request",
+    *    "status": "fail",
+    *    "error": "Your request is invalid",
     *    "message": [
     *      "title is not allowed to be empty",
     *      "title must be a string"
     *    ]
+    * } 
+    * 
+    * @example
+    * // Not found task response
+    * statusCode: 404
+    * {
+    *    "status": "fail",
+    *    "message": "Task not found"
+    * }
+    * 
+    * @example
+    * // Internal Server Error response
+    * statusCode: 500
+    * {
+    *    "status": "error",
+    *    "message": "Internal Server Error"
     * }
     */
-   // async update(id: string, updateTaskDto: UpdateTaskDto) {
-   async update(updateTaskDto: UpdateTaskDto) {
+   async update(updateTaskDto: UpdateTaskDto): Promise<Object | any> {
       try {
          const task = await this.prisma.task.findUnique({
             where: {
@@ -225,22 +263,37 @@ export class TasksService {
 
    /**
     * 
-    * @param id 
-    * @returns 
+    * @returns {Promise<Object | any>} The response contain the operation status and the message
+    * 
+    * @messagePattern tasks.delete
+    * @description Delete a task by id
     * 
     * @example
+    * // Example success response
+    * statusCode: 200
     * {
-    *     "status": "success",
-    *     "message": "The task #1234567890abcdef12345678 has been deleted"
+    *    "status": "success",
+    *    "message": "The task #1234567890abcdef12345678 has been deleted"
     * }
+    * 
     * @example
+    * // Not found task response
+    * statusCode: 404
     * {
-    *     "statusCode": 404,
-    *     "error": "Not Found",
-    *     "message": "Task not found"
+    *    "status": "fail",
+    *    "message": "Task not found"
     * }
+    * 
+    * @example
+    * // Internal Server Error response
+    * statusCode: 500
+    * {
+    *    "status": "error",
+    *    "message": "Internal Server Error"
+    * }
+    * 
     */
-   async remove(id: string) {
+   async remove(id: string): Promise<Object | any> {
       try {
          // Check if the task exists
          const task = await this.prisma.task.findUnique({
