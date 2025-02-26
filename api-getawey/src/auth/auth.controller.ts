33810a1d8f5faaa1848a7d47bef11390d103baa5
Alejandro <alejandroalvareznistal.dev@gmail.com>
2025-02-26 19:30:27 +0100

    comments: add auth controller comments

--- api-getawey/src/auth/auth.controller.ts
+++ api-getawey/src/auth/auth.controller.ts
@@ -1,64 +1,75 @@
-	import { Controller, Get, Post, Body, Inject, HttpException, HttpStatus, BadRequestException, InternalServerErrorException } from '@nestjs/common';
-	import { Services } from 'src/enums/services.enum';
-	import { ClientProxy, RpcException } from '@nestjs/microservices';
-	import { CreateUserDto } from './dto/create-user.dto';
+import { Controller, Get, Post, Body, Inject, HttpException, HttpStatus, BadRequestException, InternalServerErrorException } from '@nestjs/common';
+import { Services } from 'src/enums/services.enum';
+import { ClientProxy, RpcException } from '@nestjs/microservices';
+import { CreateUserDto } from './dto/create-user.dto';
 import { catchError, firstValueFrom, throwError } from 'rxjs';
 import { LoginUserDto } from './dto/login-user.dto';
 
-	@Controller('auth')
-	export class AuthController {
+@Controller('auth')
+export class AuthController {
 	/**
-		* 
-		* @param authService 
-		* @description Inject the auth service to communicate with the auth service
+	* 
+	* @param authService 
+	* @description Inject the auth service to communicate with the auth service
 	*/
 	constructor(@Inject(Services.AUTH_SERVICE) private readonly authService: ClientProxy) {}
 
 	/**
-		* 
-		* @returns 
-		* @description Healthcheck endpoint to check if the service is up and running
-		*/
+	* 
+	* @description Healthcheck endpoint to check if the service is up and running
+	*/
 	@Get('healt')
 	healt() {
 		return this.authService.send('auth.healt', {});
 	} 
 
 	/**
-		* 
-		* @param createUserDto 
-		* @description Create a new user
-		* @description Send the request to the auth service
-		* @description The auth service will handle the request and return the response
-		* @returns 
-		* @throws
-		* @example
-		* {
-		*     "email": "test@test.com",
-		*     "password": "test123"
-		* }
-		* @example
-		* 200
-		* {
-		*     "status": "success"
-		* }
-		* @example
-		* 400
-		* {
-		*     "message": "Your request is invalid",
-		* 	  "status": "fail",
-		*     "details": [
-		*         "password is not strong enough",
-		*         "password must be a string"
-		*     ],
-		* }
-		* @example
-		* 500
-		* {
-		*     "message": "Internal Server Error",
-		*     "status": 'error',
-		* }
-	*/
+	 * 
+	 * @route POST /auth/register
+	 * @description Create a new user
+	 * 
+	 * @param createUserDto The email and password of the user
+	 * @param createUserDto.email The email of the user
+	 * @param createUserDto.password The password of the user
+	 *
+	 * @returns {Promise<any>} The response contain the operation status and the created user
+	 * 
+	 * @response 200 {object} user - The created user
+	 * @response 400 {string} message - "Bad Request"
+	 * @response 500 {string} message - "Internal Server Error"
+	 * 
+	 * @example
+	 * // Example success response
+	 * statusCode: 200
+	 * {
+	 *     "status": "success",
+	 * 	   "token": "eyjdslkjdfl...",
+	 *     "user": {
+	 *       "email": "alex@gmail.com",
+	 *       "_id": "67b70370e5bb2f15a98aaf78",
+	 *     }  
+	 * }
+	 * 
+	 * @example
+	 * // Example bad request response
+	 * statusCode: 400
+	 * {
+	 *     "message": "Your request is invalid",
+	 * 	   "status": "fail",
+	 *     "details": [
+	 *         "password is not strong enough",
+	 *         "password must be a string"
+	 *     ],
+	 * }
+	 * 
+	 * @example
+	 * // Internal Server Error response
+	 * statusCode: 500
+	 * {
+	 *     "message": "Internal Server Error",
+	 *     "status": 'error',
+	 * }
+	 */
 	@Post('register')
 	async create(@Body() createUserDto: CreateUserDto): Promise<any> {
 		try {
@@ -78,30 +89,30 @@ import { LoginUserDto } from './dto/login-user.dto';
 
 	/**
 	 * 
-	 * @param loginUserDto
+	 * @route POST /auth/login
 	 * @description Login a user
-	 * @description Send the request to the auth service
-	 * @returns {Promise<any>}
-	 * @throws
-	 * @example
 	 * 
-	 * {
-	 * 	"email": "alex@gmail.com",
-	 * 	"password": "12345678"
-	 * }
+	 * @param loginUserDto Password and email of the user
+	 * @param loginUserDto.email The email of the user
+	 * @param loginUserDto.password The password of the user
+	 * 
+	 * @returns {Promise<any>} The response contain the operation status and the token
+	 * 
+	 * @response 200 {object} token - The token
+	 * @response 400 {string} message - "Bad Request"
+	 * @response 500 {string} message - "Internal Server Error"
 	 * 
 	 * @example
-	 * 200
+	 * // Example success response
+	 * statusCode: 200
 	 * {
 	 *     "status": "success",
 	 * 	   "token": "eyjdslkjdfl...",
-	 *     "user": {
-	 *       "email": "alex@gmail.com",
-	 *       "_id": "67b70370e5bb2f15a98aaf78",
-	 *     }  
 	 * }
+	 * 
 	 * @example
-	 * 400
+	 * // Example bad request response
+	 * statusCode: 400
 	 * {
 	 *     "message": "Your request is invalid",
 	 * 	   "status": "fail",
@@ -110,13 +121,23 @@ import { LoginUserDto } from './dto/login-user.dto';
 	 *         "password must be a string"
 	 *     ],
 	 * }
+	 * 
 	 * @example
-	 * 500
+	 * // Email or password incorrect response
+	 * statusCode: 400
+	 * {
+	 *     "message": "Email or password incorrect",
+	 * 	   "status": "fail",
+	 * }
+	 * 
+	 * @example
+	 * // Internal Server Error response
+	 * statusCode: 500
 	 * {
 	 *     "message": "Internal Server Error",
 	 *     "status": 'error',
 	 * }
-	*/
+	 */
 	@Post('login')
 	async login(@Body() loginUserDto: LoginUserDto): Promise<any> {
 		try {
