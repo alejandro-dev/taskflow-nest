28705d6574aaf3df539c079ff4b88f9b121db013
Alejandro <alejandroalvareznistal.dev@gmail.com>
2025-02-28 20:46:29 +0100

    feature: add new dto assign-author and change-status in api-getawey

---
+++ api-getawey/src/tasks/dto/assign-author.dto.ts
@@ -0,0 +1,6 @@
+import { IsString, Matches } from "class-validator";
+
+export class AssignAuthorDto {
+   @Matches(/^[0-9a-fA-F]{24}$/, { message: 'assignedTo must be a valid MongoDB ObjectId' })
+   assignedTo?: string;
+}
