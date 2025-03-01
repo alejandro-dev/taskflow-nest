4752470109ea43d050a833200792e91a89228f16
Alejandro <alejandroalvareznistal.dev@gmail.com>
2025-03-01 12:19:16 +0100

    comments: add comments

--- auth/src/users/users.service.ts
+++ auth/src/users/users.service.ts
@@ -7,9 +7,36 @@ export class UsersService {
    // Inject the user repository to communicate with the user repository
 	constructor(private readonly userRepository: UserRepository) {}
 
-   findAll() {
+   /**
+    * 
+    * @returns {Promise<Object | any>} The list of users
+    * @description Get all users with the id and email and active status
+    * 
+    * @example
+    * // Example success response
+    * statusCode: 200
+    * {
+    *    "status": "success",
+    *    "users": [
+    *      {
+    *        "id": "1234567890abcdef12345678",
+    *        "email": "alex@gmail.com"
+    *      }
+    *    ]
+    * }
+    * 
+    * @example
+    * // Internal Server Error response
+    * statusCode: 500
+    * {
+    *    "status": "error",
+    *    "message": "Internal Server Error"
+    * }
+    *
+    */
+   async findAll(): Promise<Object | any> {
       try {
-         return this.userRepository.findAll(['id', 'email']); 
+         return await this.userRepository.findAll(['id', 'email']); 
          
       } catch (error) {
          handleRpcError(error);
