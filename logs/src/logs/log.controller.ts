4a47e1abed5842ba64cb3ccb5cf1dd8f08d6b468
Alejandro <alejandroalvareznistal.dev@gmail.com>
2025-03-08 10:07:38 +0100

    comments: add coments to logs microservice

--- logs/src/logs/log.controller.ts
+++ logs/src/logs/log.controller.ts
@@ -6,8 +6,22 @@ import { LogService } from './log.service';
 export class LogController {
    constructor(private readonly logService: LogService) {}
 
+   /**
+    * 
+    * @messagePattern logs.create
+    * @param {Object} logData - The log data
+    * @param {string} logData.level - The log level (INFO, ERROR, WARN)
+    * @param {string} logData.message - The log message
+    * @param {string} logData.request_id - The request id. Identifies the request
+    * @param {string} logData.user_id - The user id
+    * @param {string} logData.event_type - The event type
+    * @param {string} logData.service_name - The service name
+    * @param {any} logData.details - The details of the log 
+    * @description Create a log
+    * 
+    */
    @MessagePattern({ cmd: 'logs.create' })
-   create(@Payload() logData: { level: string, message: string, request_id: string, user_id: string, event_type: string, details: any }) {
-      return this.logService.handleTaskLogs(logData);
+   create(@Payload() logData: { level: string, message: string, request_id: string, user_id: string, event_type: string, service_name: string, details: any }) {
+      return this.logService.createLog(logData);
    }
 }
--- logs/src/logs/log.service.ts
+++ logs/src/logs/log.service.ts
@@ -7,9 +7,20 @@ import { Model } from 'mongoose';
 export class LogService {
 	constructor(@InjectModel(Log.name) private readonly logModel: Model<Log>) {}
 
-	// Este método se ejecuta cuando recibe un mensaje de la cola "logs_queue"
-	async handleTaskLogs(logData: { level: string, message: string, request_id: string, user_id: string, event_type: string, details: any }): Promise<any> {
-	  	console.log('Log received:', logData.message);
+	/**
+    * 
+    * @param {Object} logData - The log data
+    * @param {string} logData.level - The log level (INFO, ERROR, WARN)
+    * @param {string} logData.message - The log message
+    * @param {string} logData.request_id - The request id. Identifies the request
+    * @param {string} logData.user_id - The user id
+    * @param {string} logData.event_type - The event type
+    * @param {string} logData.service_name - The service name
+    * @param {any} logData.details - The details of the log 
+    * @description Create a log
+    * 
+    */
+	async createLog(logData: { level: string, message: string, request_id: string, user_id: string, event_type: string, details: any }): Promise<any> {
 		const log = new this.logModel(logData);
 		return await log.save();
 	}
